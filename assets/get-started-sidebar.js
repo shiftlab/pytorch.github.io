93bfed0364b3755c1099ca7f472fe61e49b70031
Pat Mellon <pat@shiftlabny.com>
2020-01-22 15:58:35 -0500

    update

--- assets/get-started-sidebar.js
+++ assets/get-started-sidebar.js
@@ -1,34 +1,34 @@
 // Create the sidebar menus for each OS and Cloud Partner
 
 $([".macos", ".linux", ".windows"]).each(function(index, osClass) {
-  buildSidebarMenu(osClass);
+  buildSidebarMenu(osClass, "#get-started-locally-sidebar-list");
 });
 
 $([".alibaba", ".aws", ".microsoft-azure", ".google-cloud"]).each(function(index, cloudPartner) {
-  buildCloudSidebarMenu(cloudPartner);
+  buildSidebarMenu(cloudPartner, "#get-started-cloud-sidebar-list");
 });
 
 // On start locally page load initially show the Mac OS menu
-showSidebar("macos");
+showSidebar("macos", ".get-started-locally-sidebar li");
 
 $(["macos", "linux", "windows"]).each(function(index, osClass) {
   $("#" + osClass).click(function() {
-    showSidebar(osClass);
+    showSidebar(osClass, ".get-started-locally-sidebar li");
   })
 });
 
-// Show cloud partner side nav on click or hide side nav if already open 
+// Show cloud partner side nav on click or hide side nav if already open
 $(["alibaba", "aws", "microsoft-azure", "google-cloud"]).each(function(index, sidebarClass) {
   $("#" + sidebarClass).click(function() {
     if ($(this).parent().hasClass('open')) {
-      showCloudSidebar();
+      showSidebar(sidebarClass);
     } else {
-      showCloudSidebar(sidebarClass);
-    } 
+      showSidebar(sidebarClass, ".get-started-cloud-sidebar li");
+    }
   })
 });
 
-function buildSidebarMenu(osClass) {
+function buildSidebarMenu(osClass, test) {
   $(osClass + " > h2," + osClass + " > h3").each(function(index, element) {
     osClass = osClass.replace(".", "");
 
@@ -38,7 +38,7 @@ function buildSidebarMenu(osClass) {
     // Combine the menu item classes
     var menuItemClasses = [osClass, indentMenuItem].join(" ");
 
-    $("#get-started-locally-sidebar-list").append(
+    $(test).append(
       "<li class='" +
         menuItemClasses +
         "' style='display:none'><a href=#" +
@@ -50,47 +50,16 @@ function buildSidebarMenu(osClass) {
   });
 }
 
-function buildCloudSidebarMenu(cloudPartner) {
-  $(cloudPartner + " > h2," + cloudPartner + " > h3").each(function(index, element) {
-    cloudPartner = cloudPartner.replace(".", "");
-
-    var indentMenuItem = $(element).get(0).tagName == "H3" ? "subitem" : "";
-
-    var cloudMenuItemClasses = [cloudPartner, indentMenuItem].join(" ");
-
-    $("#get-started-cloud-sidebar-list").append(
-      "<li class='" +
-        cloudMenuItemClasses +
-        "' style='display:none'><a href=#" +
-        this.id +
-        ">" +
-        this.textContent +
-        "</a></li>"
-    );
-  });
-}
-
-function showSidebar(osClass) {
+function showSidebar(osClass, test) {
   // Hide all of the menu items at first
   // Then filter for the selected OS
 
-  $(".get-started-locally-sidebar li")
+  $(test)
     .hide()
     .filter(function() {
       return $(this)
         .attr("class")
         .includes(osClass);
-    })
-    .show();
-}
-
-function showCloudSidebar(cloudPartner) {
-  $(".get-started-cloud-sidebar li")
-    .hide()
-    .filter(function() {
-      return $(this)
-        .attr("class")
-        .includes(cloudPartner);
     })
     .show();
 }
