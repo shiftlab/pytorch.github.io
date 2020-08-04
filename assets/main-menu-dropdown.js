3f20c951ac0c9a840e23f8409f4fde2f8e407e65
Brandon Green <brandon@shiftlabny.com>
2020-08-04 09:20:09 -0400

    Add arrow for main nav ecosystem and resources dropdown

--- assets/main-menu-dropdown.js
+++ assets/main-menu-dropdown.js
@@ -1,9 +1,11 @@
 $("[data-toggle='ecosystem-dropdown']").on("click", function() {
   toggleDropdown($(this).attr("data-toggle"));
+  $(this).children("a").toggleClass("with-right-white-arrow with-down-white-arrow")
 });
 
 $("[data-toggle='resources-dropdown']").on("click", function() {
   toggleDropdown($(this).attr("data-toggle"));
+  $(this).children("a").toggleClass("with-right-white-arrow with-down-white-arrow")
 });
 
 function toggleDropdown(menuToggle) {
