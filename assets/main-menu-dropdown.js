1d2373f961b5b0117a9aa41189f07e1d044fba1e
Brandon Green <brandon@shiftlabny.com>
2020-08-13 14:22:00 -0400

    Change dropdown arrows to static down arrows

--- assets/main-menu-dropdown.js
+++ assets/main-menu-dropdown.js
@@ -1,6 +1,5 @@
 $("[data-toggle='resources-dropdown']").hover(function() {
   toggleDropdown($(this).attr("data-toggle"));
-  toggleArrowClass(".resource-option");
 });
 
 function toggleDropdown(menuToggle) {
@@ -13,10 +12,4 @@ function toggleDropdown(menuToggle) {
     $("[data-toggle=" + menuToggle + "].show-menu").removeClass(showMenuClass);
     $(menuClass).addClass(showMenuClass);
   }
-}
-
-function toggleArrowClass(dropdown, options) {
-  var options = ["with-right-white-arrow", "with-right-orange-arrow", "with-right-black-arrow"];
-  var string = "with-down-white-arrow";
-  $(dropdown).toggleClass(options + string);
 }
